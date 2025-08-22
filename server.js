6d058ac9f866e39d3c8296a01d6b498e6047fe02
Sulthan Hadil <sulusulthan230@gmail.com>
2025-08-22 12:33:24 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+let clients = [];
+
+wss.on("connection", (ws) => {
+  console.log("New client connected");
+  clients.push(ws);
+
+  ws.on("message", (message) => {
+    console.log("Received:", message.toString());
+    const stranger = clients.find((client) => client !== ws && client.readyState === ws.OPEN);
+    if (stranger) {
+      stranger.send(message.toString());
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+    clients = clients.filter((client) => client !== ws);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
